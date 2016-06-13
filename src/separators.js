06621b921599045ec28bb1438704722e95ffae67
jydawn <1198621249@qq.com>
2016-06-14 00:29:22 +0800

    提交

---
+++ src/separators.js
@@ -0,0 +1,35 @@
+<<<<<<< 8c6ef820a942bb64e46661e834913ac844b76fe0
+=======
+<<<<<<< 71af390b188ea76c17c4b1af87e3575635aed577
+>>>>>>> 提交
+'use strict';
+
+function thousands_separators(num) {
+  
+}
+
+module.exports = thousands_separators;
+<<<<<<< 8c6ef820a942bb64e46661e834913ac844b76fe0
+=======
+=======
+//'use strict';
+
+//function thousands_separators(num) {
+  
+//}
+
+//module.exports = thousands_separators;
+function thousands_separators(nStr) {
+    nStr += '';
+    var x1=x[0];
+    var x2= x.length >1?'-'+x[1]:'';
+    var rgx =/(\d+)(\d{3})/;
+    while(rgx.text (x1)){
+        x1=x1.replace(rgx,'$1'+','+'$2');
+    }
+    return x1+x2;
+    module.exports = thousands_separators;
+    alert(module.exports);
+}
+>>>>>>> 提交
+>>>>>>> 提交
